3f525fe4462a9931396a47227a5e1ac9b862da03
abdulrahman_204 <abdalrahmanaljomaat@gmail.com>
2024-11-24 21:05:04 +0300

    "Removed Old Login"

--- lib/screens/Old_Login/lib/api/api_interceptor.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-class ApiInterceptor extends Interceptor {
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    options.headers["Accepted-Lang"] = 'ar';
-    super.onRequest(options, handler);
-  }
-}
