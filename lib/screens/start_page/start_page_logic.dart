0b079dd271b0cf0312d49a300cab1449eddf8637
Mohamad Ali Alnuaimi <Mohamad Ali Alnuaimi@NGP>
2024-11-24 16:52:40 +0300

    add folders assets

--- lib/screens/start_page/start_page_logic.dart
+++ lib/screens/start_page/start_page_logic.dart
@@ -1,14 +1,9 @@
-import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
-import 'package:ngpiteapp/app/services/local_storage/cache_services.dart';
 import 'package:ngpiteapp/app/services/local_storage/cache_services_with_sharedpreferences.dart';
-import 'package:ngpiteapp/data/cache/const.dart';
 import 'package:ngpiteapp/data/enums/app_state_enum.dart';
 import 'package:ngpiteapp/data/repositories/auth_repositories.dart';
-import 'package:ngpiteapp/screens/custom_widgets/snack_bar_error.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:ngpiteapp/data/enums/loading_state_enum.dart';
-import 'package:ngpiteapp/data/module/settings_model.dart';
 
 class StartPageBinging extends Bindings {
   @override
